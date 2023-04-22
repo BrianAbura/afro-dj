3c1bb88526de645222845432891f54a8416deb94
BrianAbura <byaruhanga.abu@gmail.com>
2023-04-22 11:06:54 +0300

    More Css.

--- afro_proj/afro_app1/views.py
+++ afro_proj/afro_app1/views.py
@@ -6,3 +6,6 @@ def index(request):
     user_name = "Brian Abura"
     gender = "Male"
     return render(request, 'index.html', {'p_title': p_title, 'user_name':user_name, 'gender': gender})
+
+def register(request):
+    return render(request, 'register.html')
