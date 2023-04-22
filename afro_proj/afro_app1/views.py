91f0519e64fbf6c4abd142e0fd1944e29a003f3f
BrianAbura <byaruhanga.abu@gmail.com>
2023-04-22 08:31:35 +0300

    Css Updates.

--- afro_proj/afro_app1/views.py
+++ afro_proj/afro_app1/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     p_title = "AFRO DJANGO TEMPLATING"
-    s_title = "Links"
-    return render(request, 'index.html', {'p_title': p_title, 's_title':s_title})
+    user_name = "Brian Abura"
+    gender = "Male"
+    return render(request, 'index.html', {'p_title': p_title, 'user_name':user_name, 'gender': gender})
